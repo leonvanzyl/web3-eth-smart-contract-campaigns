4d2f13198d1af6bb89453402bde9211aea4a8b34
Leon van Zyl <leon.vanzyl@gmail.com>
2022-01-08 17:07:50 +0200

    Receive the summary

--- pages/campaigns/[address].js
+++ pages/campaigns/[address].js
@@ -6,14 +6,15 @@ import Head from "next/head";
 
 // Ethereum stuff
 import factory from "../../ethereum/factory";
+import Campaign from "../../ethereum/campaign";
 import web3 from "../../ethereum/web3";
 
 // Custom Components
 import Layout from "../../components/UI/Layout";
 
 function Show(props) {
+  // Router
   const router = useRouter();
-
   const address = router.query["address"];
 
   return (
@@ -25,5 +26,22 @@ function Show(props) {
     </Layout>
   );
 }
+
+//uses server side rendering to call the campaign contracts
+Show.getInitialProps = async (data) => {
+  const address = data.query["address"];
+  const campaign = Campaign(address);
+  const summary = await campaign.methods.getSummary().call();
+
+  return {
+    summary: {
+      minimumContribution: summary[0],
+      balance: summary[1],
+      requests: summary[2],
+      approversCount: summary[3],
+      manager: summary[4],
+    },
+  };
+};
 
 export default Show;
